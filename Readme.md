8ed7bd9206d78447fc73654fa3563413a506d3c6
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2018-07-29 14:33:21 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# password-check
+
+A simple command-line utility to check a single password against the [Pwned Passwords API](https://haveibeenpwned.com/Passwords). Might feel more comfortable to some to have easily-inspectable Rust code over entering your candidate password directly into a webpage.
+
+## Usage
+
+You will need the Rust programming language installed (should work on most recent stable versions as of July 2018). Simply execute `cargo run`, and then input the password to check when prompted.
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate reqwest;
+extern crate rpassword;
+extern crate sha1;
+
+// Securely read a password and query the Pwned Passwords API to
+// determine if it's been breached ever.
+
+fn main() {
+    let pass = rpassword::prompt_password_stdout("Password: ").unwrap();
+
+    let digest = sha1::Sha1::from(pass).digest().to_string().to_uppercase();
+    let (prefix, suffix) = (&digest[..5], &digest[5..]);
+
+    // API requires us to submit just the first 5 characters of the hash
+
+    let url = format!("https://api.pwnedpasswords.com/range/{}", prefix);
+    let mut response = reqwest::get(&url).unwrap();
+
+    let body = response.text().unwrap();
+
+    // Reponse is a series of lines like
+    // 
+    //  suffix:N
+    //
+    // Where N is the number of times that password has appeared.
+
+    for line in body.lines() {
+        let mut split = line.split(':');
+        if split.next().unwrap() == suffix {
+            println!("{} matches found.", split.next().unwrap());
+            return;
+        } 
+    }
+
+    println!("No matches found.");
+    return;
+}
